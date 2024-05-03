ed7b6c36a6b772e6eee97c6a91b050cb00928693
Jack Stechersmith <jackstechersmith@gmail.com>
2024-05-03 02:41:54 +0000

    finished lab 2

---
+++ dp2.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import pymongo
+from pymongo import MongoClient, errors
+from bson.json_util import dumps
+import json
+import os
+
+MYMONGO = os.getenv('MYMONGO')
+client = MongoClient("mongodb+srv://cluster0.v3fakhg.mongodb.net/svc8ft", username='jackstechersmith', password=MYMONGO, connectTimeoutMS=200, retryWrites=True)
+db = client.dp2
+collection = db.myimports
+
+collection.drop()
+
+directory = "data"
+
+counter = 0
+
+
+for filename in os.listdir(directory):
+    counter += 1
+    if counter > 50:
+        break
+    else:
+        with open(os.path.join(directory, filename)) as f:
+            # assuming you have defined a connection to your db and collection already:
+            # Loading or Opening the json file
+            try:
+                file_data = json.load(f)
+            except Exception as e:
+                print(e, "error when loading", f)
+
+# Inserting the loaded data in the collection
+# if JSON contains data more than one entry
+# insert_many is used else insert_one is used
+
+            if isinstance(file_data, list):
+                try:
+                    collection.insert_many(file_data)
+                except errors.DuplicateKeyError as dke:
+                    print(f"Duplicate key error for {filename}: {dke}")
+                    continue
+                except errors.PyMongoError as pme:
+                    print(f"Other MongoDB error for {filename}: {pme}")
+                except Exception as e:
+                    print(e, "when importing into Mongo")
+            else:
+                try:
+                    collection.insert_one(file_data)
+                except errors.DuplicateKeyError as dke:
+                    print(f"Duplicate key error for {filename}: {dke}")
+                    continue
+                except errors.PyMongoError as pme:
+                    print(f"Other MongoDB error for {filename}: {pme}")
+                except Exception as e:
+                    print(e)
